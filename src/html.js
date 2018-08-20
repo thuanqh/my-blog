b9ff7312b8814db0088d12cc3af896040ce87b27
Jason Quach (Huu Thuan) <thuanqh@gmail.com>
2018-08-20 19:00:38 +0700

    Update with new source code

--- src/html.js
+++ src/html.js
@@ -1,84 +1,52 @@
-import React from "react"
+/* eslint  react/prop-types: 0 */
+import React from "react";
 
-let stylesStr
+let stylesStr;
 if (process.env.NODE_ENV === `production`) {
   try {
-    stylesStr = require(`!raw-loader!../public/styles.css`)
+    stylesStr = require(`!raw-loader!../public/styles.css`);
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
 module.exports = class HTML extends React.Component {
   render() {
-    let css
+    let css;
     if (process.env.NODE_ENV === `production`) {
-      css = (
-        <style
-          id="gatsby-inlined-css"
-          dangerouslySetInnerHTML={{ __html: stylesStr }}
-        />
-      )
+      css = <style id="gatsby-inlined-css" dangerouslySetInnerHTML={{ __html: stylesStr }} />;
     }
     return (
       <html {...this.props.htmlAttributes}>
         <head>
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           {this.props.headComponents}
           {css}
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="#D0E0D8" />
+          <meta name="apple-mobile-web-app-title" content="Lazywill" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-57x57.png" sizes="57x57" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-60x60.png" sizes="60x60" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-72x72.png" sizes="72x72" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-76x76.png" sizes="76x76" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-114x114.png" sizes="114x114" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-120x120.png" sizes="120x120" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-144x144.png" sizes="144x144" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" sizes="152x152" />
+          <link rel="apple-touch-icon" href="/icons/apple-icon-180x180.png" sizes="180x180" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/icons/favicon-96x96.png" />
         </head>
         <body {...this.props.bodyAttributes}>
+          <noscript>You need to enable JavaScript to run this app!</noscript>
           {this.props.preBodyComponents}
-          <div
-            key={`body`}
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
+          <div key={`body`} id="___gatsby" dangerouslySetInnerHTML={{ __html: this.props.body }} />
           {this.props.postBodyComponents}
-          <script
-            dangerouslySetInnerHTML={{
-                __html: `
-                  document.addEventListener('DOMContentLoaded', () => {
-
-                    // Get all "navbar-burger" elements
-                    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-                  
-                    // Check if there are any navbar burgers
-                    if ($navbarBurgers.length > 0) {
-                  
-                      // Add a click event on each of them
-                      $navbarBurgers.forEach( el => {
-                        el.addEventListener('click', () => {
-                  
-                          // Get the target from the "data-target" attribute
-                          const target = el.dataset.target;
-                          const $target = document.getElementById(target);
-                  
-                          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-                          el.classList.toggle('is-active');
-                          $target.classList.toggle('is-active');
-                  
-                        });
-                      });
-                    }
-                  
-                  });
-                  
-                  
-                  window.onscroll = () => {
-                    const topnav = document.querySelector('#topnav');
-                    if(this.scrollY <= 10) topnav.className='navbar is-transparent is-fixed-top'; else topnav.className='navbar is-fixed-top scroll';
-                  };
-                `,
-            }}
-            />
         </body>
       </html>
-    )
+    );
   }
-}
+};
