1835244d96313826383f55a206e55f2737d68b09
Jason Quach <thuanqh@gmail.com>
2021-01-25 20:59:47 +0700

    Add mdx, source-filesystem and transformer

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,45 @@ module.exports = {
   pathPrefix: "/my-blog",
 };
 module.exports = {
-  plugins: ["gatsby-plugin-theme-ui"],
+  plugins: [
+    `gatsby-plugin-theme-ui`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1380,
+              linkImagesToOriginal: false,
+            },
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+          },
+          {
+            resolve: `gatsby-remark-smartypants`,
+          },
+        ],
+        remarkPlugins: [require(`remark-slug`)],
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content/assets`,
+        path: `content/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `content/posts`,
+        path: `content/posts`,
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
